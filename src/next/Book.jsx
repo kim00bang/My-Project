4dc684d26b93c65b03bdcb229f4547cc3ec08933
kim00bang <wkekrk1227@naver.com>
2023-10-31 18:22:28 +0900

    1031

--- src/next/Book.jsx
+++ src/next/Book.jsx
@@ -1,9 +1,151 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Table, Button, Form, Col, Row, Spinner, InputGroup } from 'react-bootstrap'
 
 const Book = () => {
+    const location = useLocation();
+    const search = new URLSearchParams(location.search);
+    const path = location.pathname;
+    const navi = useNavigate();
+    const page = search.get("page") ? parseInt(search.get("page")) : 1;
+    const [query, setQuery] = useState(search.get("query") ? search.get("query") : "리액트");
+    const [loading, setLoading] = useState(false);
+    const [books, setBooks] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [end, setEnd] = useState(false);
+    const [chcnt, setChcnt] = useState(0);
+
+    const getBooks = async() => {
+        const url = `https://dapi.kakao.com/v3/search/book?target=title&query=${query}&size=5&page=${page}`;
+        const config = {
+            headers: {
+                "Authorization": "KakaoAK 101f7a17f872e02b2ae8da8a87b16c1c"
+            }
+        }
+        setLoading(true);
+        const res = await axios(url, config);
+        //console.log(res.data);
+        let docs=res.data.documents;
+        docs= docs.map(doc=>doc && {...doc, checked:false});
+        setBooks(docs);
+        setTotal(res.data.meta.pageable_count);
+        setEnd(res.data.meta.is_end);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        getBooks();
+    }, [location]);
+
+    useEffect(()=> {   
+        let cnt =0;
+        books.forEach(book=>book.checked && cnt++);
+        setChcnt(cnt);
+    }, [books]);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (query == "") {
+            alert("검색어를 입력하세요.")
+        } else {
+            navi(`${path}?query=${query}&page=1`)
+        }
+    }
+
+    const onInsert = async (book) => {
+        if (window.confirm("새로운 도서를 등록하실래요?")) {
+            //console.log(book);
+            const url = "/books/insert";
+            const res = await axios.post(url, {...book, authors:book.authors.join()});
+            //console.log(res.data);
+            if(res.data==0){
+                alert("도서가 등록 되었습니다");
+            } else{
+                alert("이미 등록된 도서입니다");
+            }
+        }
+    }
+
+    const onChangeAll =(e) =>{
+        const docs=books.map(book=>book && {...book, checked:e.target.checked});
+        setBooks(docs);
+    }
+
+    const onChangeSingle =(e, isbn)=>{
+        const docs=books.map(book=>book.isbn===isbn ? {...book, checked:e.target.checked} : book);
+        setBooks(docs);
+    }
+
+    const onClickSave = async() =>{
+        if(chcnt===0){
+            alert("저장할 도서를 선택하세요");
+        }else{
+            if(window.confirm(`${chcnt}권 도서를 저장하실래요?`)){
+                let count=0;
+                for(const book of books){
+                    if(book.checked){
+                        const url='books/insert'
+                        const res= await axios.post(url,{...book, authors:book.authors.join()});
+                        if(res.data===0) count++;
+                    }
+                };
+                alert(`${count}권 저장되었습니다.`);
+                setBooks(books.map(book=>book && {...book, checked:false}));
+            }
+        }
+    }
+    if (loading) return <div className='text-center'><h1>로딩중 입니다..</h1><Spinner variant='dark' /></div>
     return (
         <div>
-            <h1>도서</h1>
+            <h1 className='my-5'>도서 검색</h1>
+            <Row>
+                <Col className='mt-3 mb-3'>검색수 : {total}권</Col>
+                <Col>
+                    <Col className='text-end'></Col>
+                </Col>
+                <Col md={6}>
+                    <form onSubmit={onSubmit} >
+                        <InputGroup>
+                            <Form.Control value={query} onChange={(e) => setQuery(e.target.value)} />
+                            <Button type='submit' variant='dark'>검색</Button>
+                        </InputGroup>
+                    </form>
+                </Col>
+            </Row>
+            <Table hover striped bordered variant='success'>
+                <thead>
+                    <tr className='text-center'>
+                        <th>이미지</th>
+                        <th>제목</th>
+                        <th>가격</th>
+                        <th>저자</th>
+                        <th>출판사</th>
+                        <th>저장</th>
+                        <th><input className='me-3' type='checkbox' checked={books.length === chcnt} onChange={onChangeAll} /><Button variant='success' size='sm' onClick={onClickSave}>선택저장</Button></th>
+                    </tr>
+                </thead>
+                <tbody className='text-center'>
+                    {books.map(book =>
+                        <tr key={book.isbn}>
+                            <img src={book.thumbnail || "http://via.placeholder.com/100x100"} width="100%" />
+                            <td>{book.title}</td>
+                            <td>{book.price}</td>
+                            <td>{book.authors}</td>
+                            <td>{book.publisher}</td>
+                            <td><Button onClick={() => onInsert(book)} variant='success'>저장</Button></td>
+                            <td><input type='checkbox' checked={book.checked} onChange={(e)=>onChangeSingle(e, book.isbn)}/></td>
+                        </tr>
+                    )}
+                </tbody>
+            </Table>
+            {total > 5 &&
+                <div className='text-center'>
+                    <Button onClick={() => navi(`${path}?query=${query}&page=${page - 1}`)} disabled={page === 1} variant='dark'>이전</Button>
+                    <span className='mx-4'>{page}/{Math.ceil(total / 5)}</span>
+                    <Button onClick={() => navi(`${path}?query=${query}&page=${page + 1}`)} disabled={end} variant='dark'>다음</Button>
+                </div>
+            }
         </div>
     )
 }
--- src/next/Home.jsx
+++ src/next/Home.jsx
@@ -13,14 +13,14 @@ import Modal_Paul from '../modal/Modal_Paul';
 
 
 const Home = () => {
-    const [box, setBox] = useState({show: false});
-    const [box1, setBox1] = useState({show: false});
-    const [box2, setBox2] = useState({show: false});
-    const [box3, setBox3] = useState({show: false});
-    const [box4, setBox4] = useState({show: false});
-    const [box5, setBox5] = useState({show: false});
-    const [box6, setBox6] = useState({show: false});
-    const [box7, setBox7] = useState({show: false});
+    const [box, setBox] = useState({ show: false });
+    const [box1, setBox1] = useState({ show: false });
+    const [box2, setBox2] = useState({ show: false });
+    const [box3, setBox3] = useState({ show: false });
+    const [box4, setBox4] = useState({ show: false });
+    const [box5, setBox5] = useState({ show: false });
+    const [box6, setBox6] = useState({ show: false });
+    const [box7, setBox7] = useState({ show: false });
     return (
         <div>
             <Container className='mb-2'>
@@ -34,29 +34,29 @@ const Home = () => {
                                         <Col>
                                             <Card>
                                                 <Card.Header><a className='black' href='https://newjeans.kr/'>NewJeans</a></Card.Header>
-                                                <Card.Img onClick={()=>setBox({show:true})} src="/img/NewJeans.jpg" width={155} height={155}/>
-                                                <Modal_NewJeans box={box} setBox={setBox}/>
+                                                <Card.Img onClick={() => setBox({ show: true })} src="/img/NewJeans.jpg" width={155} height={155} />
+                                                <Modal_NewJeans box={box} setBox={setBox} />
                                             </Card>
                                         </Col>
                                         <Col>
                                             <Card>
                                                 <Card.Header><a className='black' href='http://shofar-ent.com/html/dh/artist_view?cate_no=1-20'>최유리</a></Card.Header>
-                                                <Card.Img onClick={()=>setBox1({show:true})} src="/img/최유리.jpg" width={155} height={155}/>
-                                                <Modal_Choi box1={box1} setBox1={setBox1}/>
+                                                <Card.Img onClick={() => setBox1({ show: true })} src="/img/최유리.jpg" width={155} height={155} />
+                                                <Modal_Choi box1={box1} setBox1={setBox1} />
                                             </Card>
                                         </Col>
                                         <Col>
                                             <Card>
                                                 <Card.Header><a className='black' href='https://day6.jype.com/'>DAY6</a></Card.Header>
-                                                <Card.Img onClick={()=>setBox2({show:true})} src="/img/DAY6.jpg" width={155} height={155}/>
-                                                <Modal_Day6 box2={box2} setBox2={setBox2}/>
+                                                <Card.Img onClick={() => setBox2({ show: true })} src="/img/DAY6.jpg" width={155} height={155} />
+                                                <Modal_Day6 box2={box2} setBox2={setBox2} />
                                             </Card>
                                         </Col>
                                         <Col>
                                             <Card>
                                                 <Card.Header><a className='black' href='http://www.mysticstory.net/artist/lucy'>LUCY</a></Card.Header>
-                                                <Card.Img onClick={()=>setBox3({show:true})} src="/img/LUCY.jpg" width={155} height={155} />
-                                                <Modal_Lucy box3={box3} setBox3={setBox3}/>
+                                                <Card.Img onClick={() => setBox3({ show: true })} src="/img/LUCY.jpg" width={155} height={155} />
+                                                <Modal_Lucy box3={box3} setBox3={setBox3} />
                                             </Card>
                                         </Col>
                                     </Row>
@@ -64,29 +64,29 @@ const Home = () => {
                                         <Col>
                                             <Card>
                                                 <Card.Header><a className='black' href='https://wake-one.com/artists/ha-hyungsang-2/'>하현상</a></Card.Header>
-                                                <Card.Img onClick={()=>setBox4({show:true})} src="/img/하현상.jpg" width={155} height={155} />
-                                                <Modal_Ha box4={box4} setBox4={setBox4}/>
+                                                <Card.Img onClick={() => setBox4({ show: true })} src="/img/하현상.jpg" width={155} height={155} />
+                                                <Modal_Ha box4={box4} setBox4={setBox4} />
                                             </Card>
                                         </Col>
                                         <Col>
                                             <Card>
                                                 <Card.Header><a className='black' href='https://msbsound.com/artist/10cm/'>10CM</a></Card.Header>
-                                                <Card.Img onClick={()=>setBox5({show:true})} src="/img/10CM.jpg" width={155} height={155} />
-                                                <Modal_10cm box5={box5} setBox5={setBox5}/>
+                                                <Card.Img onClick={() => setBox5({ show: true })} src="/img/10CM.jpg" width={155} height={155} />
+                                                <Modal_10cm box5={box5} setBox5={setBox5} />
                                             </Card>
                                         </Col>
                                         <Col>
                                             <Card>
                                                 <Card.Header><a className='black' href='https://www.ygfamily.com/artist/Main.asp?LANGDIV=K&ATYPE=2&ARTIDX=52'>AKMU</a></Card.Header>
-                                                <Card.Img onClick={()=>setBox6({show:true})} src="/img/AKMU.jpg" width={155} height={155} />
-                                                <Modal_Akmu box6={box6} setBox6={setBox6}/>
+                                                <Card.Img onClick={() => setBox6({ show: true })} src="/img/AKMU.jpg" width={155} height={155} />
+                                                <Modal_Akmu box6={box6} setBox6={setBox6} />
                                             </Card>
                                         </Col>
                                         <Col>
                                             <Card>
                                                 <Card.Header><a className='black' href='https://www.instagram.com/paulkim.official/'>폴킴</a></Card.Header>
-                                                <Card.Img onClick={()=>setBox7({show:true})} src="/img/폴킴.jpg" width={155} height={155} />
-                                                <Modal_Paul box7={box7} setBox7={setBox7}/>
+                                                <Card.Img onClick={() => setBox7({ show: true })} src="/img/폴킴.jpg" width={155} height={155} />
+                                                <Modal_Paul box7={box7} setBox7={setBox7} />
                                             </Card>
                                         </Col>
                                     </Row>
@@ -97,7 +97,7 @@ const Home = () => {
                     <Col sm={4}>
                         <Card>
                             <Card.Body>로그인 하시면 또 다른 세상이 펼쳐집니다.</Card.Body>
-                            <Button variant='success'>로그인</Button>
+                            <Button className='mx-2' variant='success'>로그인</Button>
                             <Nav.Link className='mt-3 mb-3'>회원가입</Nav.Link>
                         </Card>
                         <Card>
@@ -115,51 +115,51 @@ const Home = () => {
                             <Row className='mt-2'>
                                 <Col>
                                     <Card>
-                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100}/>
+                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100} />
                                     </Card>
                                 </Col>
                                 <Col>
                                     <Card>
-                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100}/>
+                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100} />
                                     </Card>
                                 </Col>
                                 <Col>
                                     <Card>
-                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100}/>
-                                    </Card>
-                                </Col>
-                            </Row>
-                            <Row className='mt-2'>
-                                <Col>
-                                    <Card>
-                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100}/>
-                                    </Card>
-                                </Col>
-                                <Col>
-                                    <Card>
-                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100}/>
-                                    </Card>
-                                </Col>
-                                <Col>
-                                    <Card>
-                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100}/>
+                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100} />
                                     </Card>
                                 </Col>
                             </Row>
                             <Row className='mt-2'>
                                 <Col>
                                     <Card>
-                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100}/>
+                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100} />
                                     </Card>
                                 </Col>
                                 <Col>
                                     <Card>
-                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100}/>
+                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100} />
                                     </Card>
                                 </Col>
                                 <Col>
                                     <Card>
-                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100}/>
+                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100} />
+                                    </Card>
+                                </Col>
+                            </Row>
+                            <Row className='mt-2'>
+                                <Col>
+                                    <Card>
+                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100} />
+                                    </Card>
+                                </Col>
+                                <Col>
+                                    <Card>
+                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100} />
+                                    </Card>
+                                </Col>
+                                <Col>
+                                    <Card>
+                                        <Card.Img src='https://via.placeholder.com/100x100' width={100} height={100} />
                                     </Card>
                                 </Col>
                             </Row>
@@ -170,7 +170,7 @@ const Home = () => {
                             <Row>
                                 <Col>
                                     <Card>
-                                        <Card.Header>Local</Card.Header>
+                                        <Card.Header>지도</Card.Header>
                                     </Card>
                                 </Col>
                             </Row>
